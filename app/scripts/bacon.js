cc60d8a3fa31be2ea16d88faad96da383f07780a
Anuradis <p.pachocki@gmail.com>
2020-06-01 19:46:16 +0100

    task 2 - tab checkout added

--- app/scripts/bacon.js
+++ app/scripts/bacon.js
@@ -2,7 +2,8 @@ const baconBtn = document.querySelector('.bacon-btn');
 const sectionCenter = document.querySelector('.section--bacon');
 
 const addMoreBacon = () => {
-  let baconImg = document.createElement('img')
+  const baconImg = document.createElement('img')
+  baconImg.setAttribute('alt', 'Bacon');
   baconImg.setAttribute('src','http://media.washtimes.com.s3.amazonaws.com/media/image/2015/02/23/bacon.jpg');
   baconImg.style.width = '100%';
   baconImg.style.height = '100%';
