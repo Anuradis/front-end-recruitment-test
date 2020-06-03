c0448c33b43b6d5a9949a7b2a60bc9193423ca05
Anuradis <p.pachocki@gmail.com>
2020-06-03 20:27:41 +0100

    task 3 - v1

--- app/scripts/bacon.js
+++ app/scripts/bacon.js
@@ -4,10 +4,10 @@ const sectionCenter = document.querySelector('.section--bacon');
 const addMoreBacon = () => {
   const baconImg = document.createElement('img')
   baconImg.setAttribute('alt', 'Bacon');
-  baconImg.setAttribute('src','http://media.washtimes.com.s3.amazonaws.com/media/image/2015/02/23/bacon.jpg');
+  baconImg.setAttribute('src', 'http://media.washtimes.com.s3.amazonaws.com/media/image/2015/02/23/bacon.jpg');
   baconImg.style.width = '100%';
   baconImg.style.height = '100%';
   sectionCenter.appendChild(baconImg);
- }
+}
 
- baconBtn.addEventListener('click', addMoreBacon )
+baconBtn.addEventListener('click', addMoreBacon)
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -17,7 +17,7 @@
  *
  */
 /* eslint-env browser */
-(function() {
+(function () {
   'use strict';
 
   // Check to make sure service workers are supported in the current browser,
@@ -39,9 +39,9 @@
     && (window.location.protocol === 'https:' || isLocalhost)
   ) {
     navigator.serviceWorker.register('service-worker.js')
-      .then(function(registration) {
+      .then(function (registration) {
         // updatefound is fired if service-worker.js changes.
-        registration.onupdatefound = function() {
+        registration.onupdatefound = function () {
           // updatefound is also fired the very first time the SW is installed,
           // and there's no need to prompt for a reload at that point.
           // So check here to see if the page is already controlled,
@@ -51,29 +51,193 @@
             // https://slightlyoff.github.io/ServiceWorker/spec/service_worker/index.html#service-worker-container-updatefound-event
             const installingWorker = registration.installing;
 
-            installingWorker.onstatechange = function() {
+            installingWorker.onstatechange = function () {
               switch (installingWorker.state) {
-              case 'installed':
-                // At this point, the old content will have been purged and the
-                // fresh content will have been added to the cache.
-                // It's the perfect time to display a "New content is
-                // available; please refresh." message in the page's interface.
-                break;
+                case 'installed':
+                  // At this point, the old content will have been purged and the
+                  // fresh content will have been added to the cache.
+                  // It's the perfect time to display a "New content is
+                  // available; please refresh." message in the page's interface.
+                  break;
 
-              case 'redundant':
-                throw new Error('The installing ' +
+                case 'redundant':
+                  throw new Error('The installing ' +
                     'service worker became redundant.');
 
-              default:
-                  // Ignore
+                default:
+                // Ignore
               }
             };
           }
         };
-      }).catch(function(e) {
+      }).catch(function (e) {
         console.error('Error during service worker registration:', e);
       });
   }
 
   // Your custom JavaScript goes here
+  const form = document.querySelector('#form');
+  const firstName = document.querySelector('#first-name');
+  const lastName = document.querySelector('#last-name');
+  const email = document.querySelector('#email');
+  const country = document.querySelector('#country');
+  const postalCode = document.querySelector('#postal-code');
+  const phoneNumber = document.querySelector('#phone-number');
+  const creditCard = document.querySelector('#credit-card');
+  const securityCode = document.querySelector('#security-code');
+  const expirationDate = document.querySelector('#expiration-date');
+  const submitBtn = document.querySelector('.submit_button');
+  let feedback;
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    checkInputs();
+  })
+
+  const checkInputs = () => {
+    const firstNameValue = firstName.value.trim()
+    const lastNameValue = lastName.value.trim()
+    const emailValue = email.value.trim()
+    const countryValue = country.value.trim()
+    const postalCodeValue = postalCode.value.trim()
+    const phoneNumberValue = phoneNumber.value.trim()
+    const creditCardValue = creditCard.value.trim()
+    const securityCodeValue = securityCode.value.trim()
+    const expirationDateValue = expirationDate.value.trim()
+
+
+    validateFirstName(firstNameValue, firstName);
+    validateLastName(lastNameValue, lastName);
+    validateEmail(emailValue, email);
+    validateCountry(countryValue, country);
+    validatePostcode(postalCodeValue, postalCode);
+    validatePhoneNumber(phoneNumberValue, phoneNumber);
+    validateCreditCard(creditCardValue, creditCard);
+    validateSecurityCode(securityCodeValue, securityCode);
+    validateExpiryDate(expirationDateValue, expirationDate);
+
+    validateEntireForm();
+  }
+
+  const setErrorFor = (input, message) => {
+    const formControl = input.parentElement.parentElement;
+
+    if (!document.querySelector('.error-text')) {
+      const error = document.createElement('div');
+      error.innerText = message;
+      error.className = 'error-text';
+      formControl.appendChild(error);
+    }
+  }
+
+  const setSuccessFor = (input) => {
+    const inputSection = input.parentElement.parentElement;
+    if (inputSection.querySelector('.error-text') !== null) {
+      const error = inputSection.querySelector('.error-text');
+      error.remove();
+    }
+  }
+
+  const isEmail = (email) => {
+    const re = /\S+@\S+\.\S+/;
+    return re.test(email)
+  }
+
+  const validateFirstName = (firstNameValue, firstName) => {
+    if (firstNameValue.length < 3) {
+      setErrorFor(firstName, 'First name must by minimum 3 characters long');
+    } else {
+      setSuccessFor(firstName);
+    }
+  }
+
+  const validateLastName = (lastNameValue, lastName) => {
+    if (lastNameValue.length < 3) {
+      setErrorFor(lastName, 'Last name must by minimum 3 characters long');
+    } else {
+      setSuccessFor(lastName);
+    }
+  }
+
+  const validateEmail = (emailValue, email) => {
+    if (!isEmail(emailValue)) {
+      setErrorFor(email, 'Email is not valid');
+    } else {
+      setSuccessFor(email);
+    }
+  }
+
+  const validateCountry = (countryValue, country) => {
+    if (countryValue === '') {
+      setErrorFor(country, 'Country is requred');
+    } else {
+      setSuccessFor(country);
+    }
+  }
+
+  const validatePostcode = (postalCodeValue, postalCode) => {
+    if (postalCodeValue === '') {
+      setErrorFor(postalCode, 'Postal Code is requred');
+    } else {
+      setSuccessFor(postalCode);
+    }
+  };
+
+  const validatePhoneNumber = (phoneNumberValue, phoneNumber) => {
+    if (phoneNumberValue.length < 9) {
+      setErrorFor(phoneNumber, 'Phone number must have minimum 9 digits long');
+    } else {
+      setSuccessFor(phoneNumber);
+    }
+  }
+
+  const validateCreditCard = (creditCardValue, creditCard) => {
+    let filtredCardValue = creditCardValue.replace(/-|\s/g, "");
+    if (filtredCardValue.length !== 16) {
+      setErrorFor(creditCard, 'Credit card must be 16 digits long');
+    } else {
+      setSuccessFor(creditCard);
+    }
+  }
+  const validateSecurityCode = (securityCodeValue, securityCode) => {
+    if (securityCodeValue.length !== 3) {
+      setErrorFor(securityCode, 'Security code must be 3 digits long');
+    } else {
+      setSuccessFor(securityCode);
+    }
+  };
+
+  const validateExpiryDate = (expirationDateValue, expirationDate) => {
+    let filtredExpiryDate = expirationDateValue.replace(/\/|\s/g, "");
+    if (filtredExpiryDate.length !== 4) {
+      setErrorFor(expirationDate, 'Security code must be 4 characters long');
+    } else {
+      setSuccessFor(expirationDate);
+    }
+  }
+
+  const validateEntireForm = () => {
+    if (!document.querySelector('.error-text')) {
+      if (!document.querySelector('.alert.alert-success')) {
+        if (document.querySelector('.alert.alert-danger')) {
+          document.querySelector('.alert.alert-danger').remove();
+        }
+        feedback = document.createElement('div');
+        feedback.innerText = 'Your data has been submited, thank you for filling out your information!'
+        feedback.className = 'alert alert-success';
+        submitBtn.appendChild(feedback);
+        form.reset();
+      }
+      setTimeout(() => feedback.remove(), 4000)
+    } else {
+      if (!document.querySelector('.alert.alert-danger')) {
+        feedback = document.createElement('div');
+        feedback.innerText = 'Unable to submit from, please correct data';
+        feedback.className = 'alert alert-danger';
+
+        submitBtn.appendChild(feedback);
+      }
+    }
+  }
 })();
